38eb9c3b4916796db1c2ae76750158ab05ddcbca
mekermit <istow.73193843@gmail.com>
2021-07-28 17:08:10 -0500

    button update

--- songEQ.js
+++ songEQ.js
@@ -13,55 +13,17 @@ let radioEqFreq;
     }, 1000)
 } */
 
-/* radioEqValue(); */
-
 function playStopBtn() {
     if (!song.isPlaying()) {
         song.play();
         pSBtn.html('pause');
     } else {
         song.pause();
-        pSBtn.html('play');
-    }
-}
-
-function radioEqValue() {
-    rEq0 = document.getElementById('frq0');
-    rEq1 = document.getElementById('frq1');
-    rEq2 = document.getElementById('frq2');
-    rEq3 = document.getElementById('frq3');
-    rEq4 = document.getElementById('frq4');
-    rEq5 = document.getElementById('frq5');
-    rEq6 = document.getElementById('frq6');
-    rEq7 = document.getElementById('frq7');
-    rEq8 = document.getElementById('frq8');
-    rEq9 = document.getElementById('frq9');
-
-    if (rEq0.checked == true) {
-        radioEqFreq = int(rEq0.value);
-    } else if (rEq1.checked == true) {
-        radioEqFreq = int(rEq1.value);
-    } else if (rEq2.checked == true) {
-        radioEqFreq = int(rEq2.value);
-    } else if (rEq3.checked == true) {
-        radioEqFreq = int(rEq3.value);
-    } else if (rEq4.checked == true) {
-        radioEqFreq = int(rEq4.value);
-    } else if (rEq5.checked == true) {
-        radioEqFreq = int(rEq5.value);
-    } else if (rEq6.checked == true) {
-        radioEqFreq = int(rEq6.value);
-    } else if (rEq7.checked == true) {
-        radioEqFreq = int(rEq7.value);
-    } else if (rEq8.checked == true) {
-        radioEqFreq = int(rEq8.value);
-    } else if (rEq9.checked == true) {
-        radioEqFreq = int(rEq9.value);
+        pSBtn.html('play!');
     }
 }
 
 function setup() {
-
     createCanvas(400, 400);
 
     eqSong = new p5.Filter('peaking');
@@ -85,19 +47,13 @@ function setup() {
 
     fft = new p5.FFT();
 
-    pSBtn = createButton('play');
+    pSBtn = createButton('play!');
     pSBtn.mousePressed(playStopBtn);
 }
 
 function draw() {
-
-    radioEqValue();
-    /* if (radioEqFreq == 0) {
-
-    } else {
-
-    } */
-    eqSong.freq(radioEqFreq);
+    const frequency = int(document.querySelector('input[name="frequency"]:checked').value);
+    eqSong.freq(frequency);
 
     background(0);
     let waveform = fft.waveform();
@@ -114,6 +70,4 @@ function draw() {
 }
 
 function loaded() {
-    /* song.play(); */
-    /* whiteNoise.start(); */
 }
